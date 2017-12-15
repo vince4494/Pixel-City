acd797b2d740f8477e57711a1494289dc159022d
vince zipparro <zips55@sbcglobal.net>
2017-12-15 15:05:52 -0600

    added label to display progress in pull up view and added collection programmatically to pull up view. Spinner and progress label stops and starts when new pin is dropped on map.

---
+++ Pixel City/View/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  Pixel City
+//
+//  Created by vince zipparro on 12/15/17.
+//  Copyright © 2017 vince zipparro. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell
+{
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
